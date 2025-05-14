744b1737eded5faab2f91ac4ee07fb601fb4e02a
Ayush Mishra <mishraayu2171@gmail.com>
2025-05-14 12:38:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MentalHealthSlide from './components/landing/MentalHealthSlide'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+   <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
+
+    <Navbar/>
+    {/* <MentalHealthSlide/> */}
+    </div>
+
+  )
+}
+
+export default App
