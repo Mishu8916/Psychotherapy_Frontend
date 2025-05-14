52755511325d275e8874556409595fb6ad00a002
Ayush Mishra <mishraayu2171@gmail.com>
2025-05-14 18:56:09 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base:process.env.VITE_BASE_PATH || "/Coders-Gym" ,
+   base:process.env.VITE_BASE_PATH || "/Psychotherapy_Frontend" ,
 })
